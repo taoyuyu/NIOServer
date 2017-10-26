d7939ae297624e4b833ef18e0beced124c15376c
taoyuyu <taoyu@whu.edu.cn>
2017-10-26 18:32:56 +0800

    '启动服务日志打印'

--- src/main/java/com/whu/yves/main/Main.java
+++ src/main/java/com/whu/yves/main/Main.java
@@ -1,19 +1,22 @@
 package com.whu.yves.main;
 
-import com.whu.yves.server.ActionHandler;
 import com.whu.yves.server.PushActionHandler;
 import com.whu.yves.server.SelectorFactory;
 import com.whu.yves.timing.ThreadPoolService;
+
+import org.apache.log4j.Logger;
 
 /**
  * Created by yutao on 17/9/13.
  */
 public class Main {
-
+  private static Logger LOG = Logger.getLogger(Main.class);
   public static void main(String[] args) {
     //创建推送线程
     ThreadPoolService.initPool(2);
+    LOG.info("Start push thread succeed!");
     //开启NIOServer服务
     new PushActionHandler(SelectorFactory.getSelector(7681)).listen();
+    LOG.info("Start Socket listener succeed!");
   }
 }

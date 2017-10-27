3fda8f323abe767d6fd4db26c92033791ddcf729
taoyuyu <taoyu@whu.edu.cn>
2017-10-27 20:17:14 +0800

    脚本加载配置文件

--- startup.sh
+++ startup.sh
@@ -1,1 +1,12 @@
-exec java -classpath ./target/NIOServer-1.0-SNAPSHOT.jar com.whu.yves.main.Main
+#/bin/sh
+CLASSPATH=./NIOServer-1.0-SNAPSHOT.jar:./../conf
+
+for i in ../lib/*.jar;do 
+    CLASSPATH=$CLASSPATH:$i
+done
+
+export CLASSPATH=:$CLASSPATH
+
+exec java -classpath .:${CLASSPATH} com.whu.yves.main.Main
+
+#exec java -classpath NIOServer-1.0-SNAPSHOT.jar com.whu.yves.main.Main

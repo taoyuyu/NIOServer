1050dd7613cf938337635d1acdfac87cc500380b
taoyuyu <taoyu@whu.edu.cn>
2017-11-02 16:48:36 +0800

    #1 连接池测试

---
+++ src/test/java/com/whu/yves/message/TestMessagePool.java
@@ -0,0 +1,20 @@
+package com.whu.yves.message;
+
+import org.junit.Test;
+
+/**
+ * Created by yutao on 17/11/2.
+ */
+public class TestMessagePool {
+  @Test
+  public void testPool() {
+    MessagePool.init();
+    MessagePool.addOneMessage("taoyu", "this is a message1");
+    MessagePool.addOneMessage("taoyu", "this is a message2");
+    String message;
+    while ((message = MessagePool.getOneMessageByID("taoyu"))!=null) {
+      System.out.println(message);
+    }
+    System.out.println(MessagePool.getOneMessageByID("taoyu1"));
+  }
+}

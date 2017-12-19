31255a346bd2b37296d97e6f5832229223cb46d4
taoyuyu <taoyu@whu.edu.cn>
2017-12-19 15:40:42 +0800

    删除冗余包

--- src/main/java/com/whu/yves/server/task/ThreadPoolService.java
+++ src/main/java/com/whu/yves/server/task/ThreadPoolService.java
@@ -1,6 +1,5 @@
 package com.whu.yves.server.task;
 
-import com.mysql.jdbc.TimeUtil;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;

c426a7473bf43b7749d3cc16069340b2942ac826
taoyuyu <taoyu@whu.edu.cn>
2018-01-12 21:24:56 +0800

    #16 关闭浏览器，取消请求转发任务

--- src/main/java/com/whu/yves/server/ChatActionHandler.java
+++ src/main/java/com/whu/yves/server/ChatActionHandler.java
@@ -7,6 +7,9 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
 import org.apache.log4j.Logger;
 
 /**
@@ -16,6 +19,7 @@ import org.apache.log4j.Logger;
 public class ChatActionHandler extends ActionHandler {
 
   private static Logger LOG = Logger.getLogger(ChatActionHandler.class);
+  private static ConcurrentHashMap<Integer, Future<?>> tasks = new ConcurrentHashMap<Integer, Future<?>>();
   private int BYTE_BUFFER_SIZE = 1024;
 
   public ChatActionHandler(Selector selector) {
@@ -30,9 +34,17 @@ public class ChatActionHandler extends ActionHandler {
     try {
       int count = channel.read(buffer);
       if (count > 0) {
-        ThreadPoolService.submit(new MessageHandleTask(channel, buffer));
+        Future<?> future = ThreadPoolService.submit(new MessageHandleTask(channel, buffer));
+        tasks.put(channel.socket().hashCode(), future);
       } else {
+        LOG.info(channel.socket().hashCode() + " closed");
         channel.close();
+        Future<?> future = tasks.get(channel.socket().hashCode());
+        if (future != null) {
+          future.cancel(true);
+          tasks.remove(channel.socket().hashCode());
+          LOG.info("cancel a task");
+        }
       }
     } catch (IOException e) {
       LOG.error("close channel error: " + e.getMessage());
